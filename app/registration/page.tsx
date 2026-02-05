c819093bd415604f6e109467f0ff0bc239657f26
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2026-02-05 19:18:35 +0300

    5 new fix vercel error

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,4 +1,5 @@
 import RegistrationForm from "@/components/ui/registration-form"
+import { Suspense } from "react"
 
 export default function Page(){
 
@@ -6,8 +7,10 @@ export default function Page(){
         <main className="flex mt-[5%] justify-center md:h-screen">
             <div className="relative mx-auto flex w-full max-w-[800px] flex-col space-y-2.5 p-4 ">
                
-               
+               <Suspense>
                 <RegistrationForm/>
+               </Suspense>
+                
                
             </div>
         </main>
