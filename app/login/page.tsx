3c510e3dbc2093190ba0d9dcb446a7d3c46cf60f
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2026-02-05 19:16:45 +0300

    4 new fix vercel error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,16 @@
 
 import LoginForm from '@/components/ui/login-form';
-
+import { Suspense } from 'react';
  
 export default function LoginPage() {
   return (
     <main className="flex mt-[5%] justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[800px] flex-col space-y-2.5 p-4 ">
         
-        
+        <Suspense>
           <LoginForm />
+        </Suspense>
+          
         
       </div>
     </main>
